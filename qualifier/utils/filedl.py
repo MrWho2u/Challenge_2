89a494c434d2d2c9b3db784376cbffead8046eb3
michael_roth <michael.ce.roth@gmail.com>
2022-12-27 14:36:28 -0800

    adding this module which will download the csv file to the users desired folder

---
+++ qualifier/utils/filedl.py
@@ -0,0 +1,14 @@
+# This function is designed to be a helper function that will download a list of loans for the user
+
+import csv
+
+def download_csv(save_path, qualifying_loans):
+    header = ["Lender", "Max Loan", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
+
+    with open(save_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow(header)
+
+        for row in qualifying_loans:
+            csvwriter.writerow(row.values())
